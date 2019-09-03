81d8d42fb1fa7dae2c21d1dfafa8da5fcddbf63e
poi1924 <45979451+poi1924@users.noreply.github.com>
2019-09-03 11:02:32 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,6 @@
 <?php
-$access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'NHtmYtJsUVAvNMmNdfEDYsJIf6VZ9pte4tHb9isBNjZdjK3gcslipEfkvrDI1d//i+BcyVpnYo5o+5jnJnRHD9U4KgM4T5Xt2JIeua7N+aLuNkG07euhi0r14I1mKRFWLQ0cgTWPVMEu0pWkEUH9sAdB04t89/1O/w1cDnyilFU=
+';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
