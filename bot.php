7ebdb76fcf6707fa9bb1cf02b0ddfd0ee6027f76
poi1924 <45979451+poi1924@users.noreply.github.com>
2019-09-11 23:37:12 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -15,7 +15,16 @@ if (!is_null($events['ESP'])) {
 if (!is_null($events['events'])) {
 	echo "line bot";
 	// Loop through each event
-
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+			// Build message to reply back
+			$Topic = "NodeMCU1" ;
+			getMqttfromlineMsg($Topic,$text);
 			   
 			
 		}
