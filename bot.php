e0e483ef6868295f21bbcef893ffc3901b1ed3d3
poi1924 <45979451+poi1924@users.noreply.github.com>
2019-09-11 23:37:35 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -13,8 +13,7 @@ if (!is_null($events['ESP'])) {
 	echo "OK";
 	}
 if (!is_null($events['events'])) {
-	echo "line bot";
-	// Loop through each event
+
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
