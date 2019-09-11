ec4c331924610a45cedb9157772029d06f423737
poi1924 <45979451+poi1924@users.noreply.github.com>
2019-09-11 23:43:44 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -22,30 +22,5 @@
     }
   }
  
-  function put($url,$tmsg)
-{
-      
-    $ch = curl_init($url);
- 
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-     
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-     
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-     
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-     
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $tmsg);
- 
-    //curl_setopt($ch, CURLOPT_USERPWD, "mJ7K4MfteC7p0dW:pp4gzMhCvJIqlxc66hKEvk46m");
-     
-    $response = curl_exec($ch);
-    
-      curl_close($ch);
-      echo $response . "\r\n";
-    return $response;
-}
-// $Topic = "NodeMCU1";
- //$lineMsg = "CHECK";
- //getMqttfromlineMsg($Topic,$lineMsg);
+
 ?>
